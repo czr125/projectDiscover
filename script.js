6fde57840d0b5c3957ade0b9932275b299fd85ca
czr_dev <cezar.holanda@outlook.com>
2023-08-29 13:49:58 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    
+    const img = document.querySelector("#profile img")
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', 'assets/assets/avatar-light.png'),
+        img.setAttribute('alt', '@czr_mutran image in light mode')
+    } else {
+        img.setAttribute('src', 'assets/assets/avatar.png')
+        img.setAttribute('alt', '@czr_mutran image in dark mode')
+    }
+}
+
+
